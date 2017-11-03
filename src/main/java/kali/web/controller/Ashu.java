44d07b66376ab64396b9844caa6e64120e205209
shanirajkali <shanirajkali@gmail.com>
2017-11-03 17:44:51 +0530

    Ashu controller changed

--- src/main/java/kali/web/controller/Ashu.java
+++ src/main/java/kali/web/controller/Ashu.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import kali.dao.repository.TagRepository;
 
 @RestController
-@RequestMapping("/au")
+@RequestMapping("/ashu")
 public class Ashu {
 	
 	@Autowired
