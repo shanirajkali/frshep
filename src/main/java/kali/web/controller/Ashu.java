2f0618b3a8ec02983b9858b3d1630d8e5a79a468
shanirajkali <shanirajkali@gmail.com>
2017-11-03 17:29:33 +0530

    hg

--- src/main/java/kali/web/controller/Ashu.java
+++ src/main/java/kali/web/controller/Ashu.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import kali.dao.repository.TagRepository;
 
 @RestController
-@RequestMapping("/ashu")
+@RequestMapping("/au")
 public class Ashu {
 	
 	@Autowired
